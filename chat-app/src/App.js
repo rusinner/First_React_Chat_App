59e08377e3dcab3d40db2c2ccbd33336a633d80b
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-21 14:00:11 +0200

    zetup

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import { ChatEngine } from "react-chat-engine";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">App</div>;
 }
 
 export default App;
