599bd4644b266a2eaebab0d0952ed3a149d78c39
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-21 15:51:06 +0200

    components

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 
+import ChatFeed from "./components/ChatFeed";
+
 function App() {
   return (
     <ChatEngine
@@ -8,6 +10,7 @@ function App() {
       projectID={process.env.REACT_APP_PROJECT_ID}
       userName={process.env.REACT_APP_USERNAME}
       userSecret={process.env.REACT_APP_USERSECRET}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 }
